c76ef797834f514cd1beef08c3f735c403e605fd
BarbeChao <1209436661@qq.com>
2022-10-26 18:52:34 +0800

    第三次，修改启动类

--- spring-boot-novels/src/main/java/com/luochao/SpringbootnovelsApplication.java
+++ spring-boot-novels/src/main/java/com/luochao/SpringbootnovelsApplication.java
@@ -1,9 +1,11 @@
 package com.luochao;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.luochao.dao")
 public class SpringbootnovelsApplication {
 
     public static void main(String[] args) {
