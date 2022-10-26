ab1d60dc1e95171f4ae537c5462eb362e66500eb
BarbeChao <1209436661@qq.com>
2022-10-26 18:49:43 +0800

    第一次，提交配置

---
+++ spring-boot-novels/src/main/java/com/luochao/SpringbootnovelsApplication.java
@@ -0,0 +1,13 @@
+package com.luochao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootnovelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootnovelsApplication.class, args);
+    }
+
+}
---
+++ spring-boot-novels/src/test/java/com/luochao/SpringbootnovelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luochao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootnovelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
